1da777c1fb109b6a62b53ee1aad4fd87411bff0f
Timo Kluck <tkluck@infty.nl>
2019-03-24 22:49:52 +0100

    Export getpotential

--- src/LandauGinzburgCategories.jl
+++ src/LandauGinzburgCategories.jl
@@ -8,11 +8,13 @@ include("Library.jl")
 
 import .Operations: ⨷, ⨶, fuse, fuse_abstract
 import .Operations: dual, unit_matrix_factorization
+import .Operations: getpotential
 import .OrbifoldEquivalence: quantum_dimension, quantum_dimensions
 import .QuasiHomogeneous: find_quasihomogeneous_degrees, quasidegree, centralcharge
 
 export ⨷, ⨶, fuse, fuse_abstract
 export dual, unit_matrix_factorization
+export getpotential
 export quantum_dimension, quantum_dimensions
 export find_quasihomogeneous_degrees, quasidegree, centralcharge
 
