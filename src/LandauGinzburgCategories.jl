0b5b960ab39a44d885966f5edecb1f6cc7130d2a
Timo Kluck <tkluck@infty.nl>
2020-02-20 01:31:54 +0100

    Export search_orbifold_equivalence

--- src/LandauGinzburgCategories.jl
+++ src/LandauGinzburgCategories.jl
@@ -10,13 +10,13 @@ include("Library.jl")
 import .Operations: ⨷, ⨶, fuse, fuse_abstract
 import .Operations: dual, unit_matrix_factorization
 import .Operations: getpotential
-import .OrbifoldEquivalence: quantum_dimension, quantum_dimensions
+import .OrbifoldEquivalence: quantum_dimension, quantum_dimensions, search_orbifold_equivalence
 import .QuasiHomogeneous: Gradings, find_quasihomogeneous_degrees, quasidegree, centralcharge
 
 export ⨷, ⨶, fuse, fuse_abstract
 export dual, unit_matrix_factorization
 export getpotential
-export quantum_dimension, quantum_dimensions
+export quantum_dimension, quantum_dimensions, search_orbifold_equivalence
 export Gradings, find_quasihomogeneous_degrees, quasidegree, centralcharge
 
 import PolynomialRings: @ring!, @ring
